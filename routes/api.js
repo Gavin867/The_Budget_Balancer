28184613cd65bb7914c7c523c55961fca5f2b9b4
Gavin Calkins <gavin.calkins@gmail.com>
2021-02-24 14:48:37 -0800

    route updates

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,7 @@ router.post("/api/transaction", ({body}, res) => {
     });
 });
 
+// needs to be called with 'navigator'
 router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
