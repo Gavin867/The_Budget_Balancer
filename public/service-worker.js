566e72728bb51dbed246e94888f111caeea590a6
Gavin Calkins <gavin.calkins@gmail.com>
2021-02-25 00:04:23 -0800

    issue with service worker identified

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,16 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
-  "/manifest.webmanifest",
+  "./index.html",
+  "./db.js",
+  "./service-worker.js",
+  "./manifest.webmanifest",
+  "./assets/scripts/index.js",
   "./assets/styles/styles.css",
   "/assets/images/icons/icon-192x192.png",
   "/assets/images/icons/icon-512x512.png",
-  "./assets/scripts/index.js"
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
 self.addEventListener("install", function (evt) {
