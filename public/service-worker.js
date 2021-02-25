3fb54f7e880e444eb936daac9319fc2d391e2b36
Gavin Calkins <gavin.calkins@gmail.com>
2021-02-25 00:10:32 -0800

    issue with the service worker fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,6 +14,9 @@ const FILES_TO_CACHE = [
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
