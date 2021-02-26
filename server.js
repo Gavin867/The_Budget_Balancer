2d59415c77ebc19271b32feb4fb6cc93b3068b6a
Gavin Calkins <gavin.calkins@gmail.com>
2021-02-26 01:59:43 -0800

    server connection updates

--- server.js
+++ server.js
@@ -15,18 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
