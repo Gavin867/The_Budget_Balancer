0ba0f0473679dd676e2721948d842818ce72eacd
Gavin Calkins <gavin.calkins@gmail.com>
2021-02-27 13:03:49 -0800

    adjusted file pathing on lines 11 and 12

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,8 +8,8 @@ const FILES_TO_CACHE = [
   "./manifest.webmanifest",
   "./assets/scripts/index.js",
   "./assets/styles/styles.css",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-512x512.png",
+  "./assets/images/icons/icon-192x192.png",
+  "./assets/images/icons/icon-512x512.png",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
