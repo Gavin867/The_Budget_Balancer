bb6c485eedf04f0733590aaf910cdef7c1356a97
Gavin Calkins <gavin.calkins@gmail.com>
2021-02-27 12:40:48 -0800

    updated server message in terminal

--- server.js
+++ server.js
@@ -25,6 +25,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-  console.log(`Use http://localhost:3000/ to launch`);
+  console.log(`Server listening on: http://localhost:${PORT}`);
 });
