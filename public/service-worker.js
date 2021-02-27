48984e3f07b07989ef4f7f68d71967a789880329
Gavin Calkins <gavin.calkins@gmail.com>
2021-02-27 12:50:45 -0800

    multi declaration of cache name error fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,9 +14,6 @@ const FILES_TO_CACHE = [
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
